6290a8a97d7d9edfb00b722800b7b8eea82efbc8
Glauco Pater <>
2020-07-18 11:51:52 +0200

    Add About page. Refactor Button Icon

---
+++ src/pages/about.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import { Link } from 'gatsby';
+import { Layout, Wrapper, Header, Content, SectionTitle } from '../components';
+
+import config from '../../config/SiteConfig';
+import { ButtonIcon } from '../components/ButtonIcon';
+
+export default () => {
+  return (
+    <Layout>
+      <Helmet title={`Contact | ${config.siteTitle}`} />
+      <Header>
+        <Link to="/">{config.siteTitle}</Link>
+        <SectionTitle uppercase={true}>About</SectionTitle>
+      </Header>
+      <Wrapper>
+        <Content>
+          <p>Super cool intro text to get people contacting me!</p>
+          <a href="https://www.twitter.com/mhadaily">
+            <ButtonIcon big={true}>Twitter</ButtonIcon>
+          </a>
+          <a href="https://www.linkedin.com/in/mhadaily">
+            <ButtonIcon big={true}>Linkedin</ButtonIcon>
+          </a>
+          <a href="https://www.majidhajian.com">
+            <ButtonIcon big={true}>WebSite</ButtonIcon>
+          </a>
+        </Content>
+      </Wrapper>
+    </Layout>
+  );
+};
